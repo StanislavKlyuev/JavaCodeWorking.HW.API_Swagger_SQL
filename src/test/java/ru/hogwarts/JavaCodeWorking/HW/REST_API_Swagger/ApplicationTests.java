5d6c5a9a3c090c23df32eebe874d5b172e54afe0
StanislavKlyuev <patriek@rambler.ru>
2026-03-11 11:50:09 +0300

    Initial Commit

---
+++ src/test/java/ru/hogwarts/JavaCodeWorking/HW/REST_API_Swagger/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.JavaCodeWorking.HW.REST_API_Swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
